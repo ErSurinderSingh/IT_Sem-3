1217ab7e2fae72889fa4af9736f18185c8d29104
Er. Surinder Singh <66181384+ErSurinderSingh@users.noreply.github.com>
2020-12-06 16:42:10 +0530

    Add files via upload

--- IT_Sem3/src/Practicals/AppletsAddition.java
+++ IT_Sem3/src/Practicals/AppletsAddition.java
@@ -4,65 +4,51 @@ import java.awt.Graphics;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class AppletsAddition extends JApplet {
-
-    public void paint(Graphics g) {        
-        
-    }
-    public void init(){
-        add(new UserControls());
-    }
-
-}
-
-class UserControls extends JFrame implements ActionListener{
-
+public class AppletsAddition extends JApplet implements ActionListener {
+    
     JLabel l1, l2;
-    JTextField txt_num1, txt_num2;
+    JTextField txt_num1, txt_num2,txt_res;
     JButton btn_Add;
 
-    public UserControls(){
-        l1 = new JLabel("Enter First Number: ");
-        l1.setBounds(20, 50, 150, 100);
-        add(l1);
-        
+    public void paint(Graphics g) {        
+        g.drawString("Enter First Number: ",  80, 100);
+        g.drawString("Enter Second Number: ", 80, 150);
+        g.drawString("Result: ", 150, 200);
+    }
+    public void init(){
+               
         txt_num1 = new JTextField();
         txt_num1.setBounds(200, 90, 120, 30);
         add(txt_num1);
-        
-        l2 = new JLabel("Enter Second Number: ");
-        l2.setBounds(20, 110, 150, 100);
-        add(l2);
-            
+                 
         txt_num2 = new JTextField();
         txt_num2.setBounds(200, 150, 120, 30);
         add(txt_num2);
         
-        btn_Add = new JButton("ADD");
-        btn_Add.addActionListener(this);
-        btn_Add.setBounds(200, 210, 120, 50);
-        add(btn_Add);
+        txt_res = new JTextField();
+        txt_res.setBounds(200, 210, 120, 30);
+        txt_res.setEditable(false);
+        add(txt_res);
         
+        btn_Add = new JButton("ADD");
+        add(btn_Add);
+        btn_Add.addActionListener((ActionListener) this);
+        btn_Add.setBounds(200, 270, 120, 50);
+       
         setVisible(true);
         setSize(500,500);
         setLayout(null);
-        
-        
     }
     
-    
-    @Override
     public void actionPerformed(ActionEvent e) {
         //To change body of generated methods, choose Tools | Templates.
         int num1 = Integer.parseInt(txt_num1.getText());
         int num2 = Integer.parseInt(txt_num2.getText());
-        int k = num1 + num2;
+        Integer k = num1 + num2;
+        txt_res.setText(k.toString());
         JOptionPane.showMessageDialog(this, k, "Arithmetic Operations:", JOptionPane.INFORMATION_MESSAGE);    }
-    
-    
-   
-}
 
+}
 
 /*<applet code="AppletsAddition.class" width="300" height="300">  
 </applet>  */
